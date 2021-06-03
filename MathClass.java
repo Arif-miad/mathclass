900c1c6ee6b1733b9e7cc5cb0e97f1ca8405fb27
Arif <arifadi952@gmail.com>
2021-06-03 14:48:32 -0700

    This is math class

---
+++ MathClass.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.mavenproject1;
+
+
+public class MathClass {
+    public static void main(String[] args) {
+        int x = 20;
+        int y= 30;
+        System.out.println(Math.max(x, y));
+    }
+}
